a8cd4d219a133ad68ced78e90f55b044794ea821
JarvisRAs <ras4jarvis@gmail.edu>
2021-09-03 14:30:11 -0600

    change to size on initialized array to avoid error

--- yroots/subdivision.py
+++ yroots/subdivision.py
@@ -153,7 +153,7 @@ def solve(funcs, a, b, rel_approx_tol=1.e-15, abs_approx_tol=1.e-12,
     interval_data = IntervalData(a, b, intervalReductions)
     root_tracker = RootTracker()
     values_arr.memo = {}
-    initialize_values_arr(dim,2*(deg+3))
+    initialize_values_arr(dim, 2*(deg+3))
 
     if dim == 1:
         # In one dimension, we don't use target_deg; it's the same as deg
